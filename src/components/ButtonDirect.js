32c13f5136fd79f146ead88a6d00cbb33af1134e
karthik-kiran-29 <karthikkiran29@gmail.com>
2024-11-09 22:19:23 +0530

    project setup done

---
+++ src/components/ButtonDirect.js
@@ -0,0 +1,11 @@
+import {React} from 'react';
+
+export default function ButtonDirect(){
+    return (
+        <>
+         <button className="bg-green-600 m-2 text-white font-semibold py-2 px-4 rounded-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
+            Start Creating 
+         </button>
+        </>
+    )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import {React} from 'react';
+import ButtonDirect from './ButtonDirect';
+
+export default function Home(){
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-white text-center px-6 py-10  ">
+        <h1 className="text-4xl font-bold text-green-600 mb-4">DonateToYou</h1>
+        <p>Generate social media shareables which gets donations directly to UPI ID's</p>
+        <ButtonDirect />
+        </div>
+    )
+}
